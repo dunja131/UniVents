33e77fe1e2956d0a3807143ed6b5dd60892ff0c9
Sakshi Pal <sakshipal55@gmail.com>
2026-04-03 13:07:19 +1300

    Project setup with dependencies. Done so far: created backend with springboot initializer, downloaded java extension pack + JDK so we have runnable java code, tested app in port 8080 and confirmed it works, also added spring-boot-devtools in dependencies so we cause reload page to show new pages instead of executing and aborting run command. git remote add origin https://github.com/dunja131/UniVents.git git remote -v git pull origin main --allow-unrelated-histories git push origin main git remote add origin https://github.com/dunja131/UniVents.git git remote -v pwd git statu

---
+++ src/main/java/com/UniVents/EventsManagement/Controllers/UserController.java
@@ -0,0 +1,23 @@
+package com.UniVents.EventsManagement.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+// controls user related requests in login and signup. 
+@RestController
+public class UserController {
+       @GetMapping("/hello")    
+    public String hello() {
+        return "wooww World!";
+    }
+ 
+}
+
+
+
+// done so far - made backend project with springboot initializer using maven
+// downloaded extension pack for java + jdk extensions so it imports packages and functions as an IDE (vscode is just text editor)
+// ./mvnw spring-boot:run to run app 
+// go to http://localhost:8080 to access.
+// added spring-boot-devtools in dependencies so can just reload page and it will update changes (otherwise have to end run command and repaste it.)  
---
+++ src/main/java/com/UniVents/EventsManagement/EventsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.UniVents.EventsManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsManagementApplication.class, args);
+	}
+
+}
