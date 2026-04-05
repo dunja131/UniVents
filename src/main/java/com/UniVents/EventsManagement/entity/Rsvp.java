e9c1a4805af6a6d188931b608dbcc8bed5a30c1d
dkp333 <dkpedebone@gmail.com>
2026-04-05 18:37:30 +1200

    Added primary key to Rsvp entity

--- src/main/java/com/UniVents/EventsManagement/entity/Rsvp.java
+++ src/main/java/com/UniVents/EventsManagement/entity/Rsvp.java
@@ -1,3 +1,14 @@
 package com.UniVents.EventsManagement.entity;
 
 import jakarta.persistence.*;
+
+@Entity
+@Table(name = "rsvps")
+public class Rsvp{
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name = "rsvp_id")
+private Long rsvpId;
+
+}
