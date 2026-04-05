9edc74da95b080edb53f776095084ea69b2e4cc2
dkp333 <dkpedebone@gmail.com>
2026-04-05 13:58:50 +1200

    Added primary key to Event entity

--- src/main/java/com/UniVents/EventsManagement/entity/Event.java
+++ src/main/java/com/UniVents/EventsManagement/entity/Event.java
@@ -1,3 +1,11 @@
 package com.UniVents.EventsManagement.entity;
 
 import jakarta.persistence.*;
+
+@Entity
+@Table(name = "events")
+public class Events {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+}
