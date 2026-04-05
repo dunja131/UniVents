1a3b474cec920572c0ba8e95008cdb3b90f39243
dkp333 <dkpedebone@gmail.com>
2026-04-05 16:54:32 +1200

    Added column mappings to time fields

--- src/main/java/com/UniVents/EventsManagement/entity/Event.java
+++ src/main/java/com/UniVents/EventsManagement/entity/Event.java
@@ -4,8 +4,8 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "events")
-public class Events {
+@Table(name = "event")
+public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long eventId;
@@ -19,10 +19,13 @@ public class Events {
    @Column(name = "location", nullable = false) 
     private String location; 
 
+    @Column(name = "start_time", nullable = false)
     private LocalDateTime startTime;
 
+    @Column(name = "end_time", nullable = false)
     private LocalDateTime endTime;
 
+    @Column(name = "created_at, nullable = false")
     private LocalDateTime createdAt;
 
 
