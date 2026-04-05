70342ed7ca405b1968bc5ad3418a9415cffa4db8
dkp333 <dkpedebone@gmail.com>
2026-04-05 14:05:57 +1200

    Added column mappings to basic fields

--- src/main/java/com/UniVents/EventsManagement/entity/Event.java
+++ src/main/java/com/UniVents/EventsManagement/entity/Event.java
@@ -7,5 +7,16 @@ import jakarta.persistence.*;
 public class Events {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    
+    private Long eventId;
+
+    @Column(name = "event_name", nullable = false)
+    private String eventName;
+
+   @Column(name = "description", nullable = false)
+    private String description;
+
+   @Column(name = "location", nullable = false) 
+    private String location;
+
+
 }
