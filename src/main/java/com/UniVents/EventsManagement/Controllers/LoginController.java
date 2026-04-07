87c49e967aa34f1b30d4ed13974e4ea498a579cc
Sakshi Pal <sakshipal55@gmail.com>
2026-04-07 20:05:36 +1200

    added login and logout feature to homepage

---
+++ src/main/java/com/UniVents/EventsManagement/Controllers/LoginController.java
@@ -0,0 +1,13 @@
+package com.UniVents.EventsManagement.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+    @GetMapping("/")
+public String homepage(){
+return "Welcome to UniVents";
+}
+
+}
