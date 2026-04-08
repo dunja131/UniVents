7609a6a582bd4466487ddd276553fac1a95589e3
Sakshi Pal <sakshipal55@gmail.com>
2026-04-08 22:11:36 +1200

    test commit 2

---
+++ src/main/java/com/UniVents/EventsManagement/Service/AuthService.java
@@ -0,0 +1,16 @@
+// package com.UniVents.EventsManagement.Service;
+
+// import org.springframework.security.core.context.SecurityContextHolder;
+// import org.springframework.stereotype.Service;
+// import com.UniVents.EventsManagement.entity.User;
+// import org.springframework.security.core.Authentication;
+
+// @Service
+// public class AuthService {
+    
+
+
+//     public User getCurrentUser(){
+//         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//             }
+// }
