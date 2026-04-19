77ef1fc8de87e326c833786b974226b4f6a868fa
Anna Lloyd <lloan016@student.otago.ac.nz>
2026-04-19 16:52:15 +1200

    fixing the things i broke

--- frontend/lib/services/user_service.dart
+++ frontend/lib/services/user_service.dart
@@ -11,7 +11,7 @@ class UserService {
   static final String _auth = 'Basic ${base64.encode(utf8.encode('$_username:$_password'))}';
 
   Future<List<User>> getUsers() async {
-    final response = await http.get(Uri.parse('$_baseUrl/events'), headers: <String, String>{'Authorization': _auth});
+    final response = await http.get(Uri.parse('$_baseUrl/users'), headers: <String, String>{'Authorization': _auth});
 
   if(response.statusCode == 200) {
     return userFromJson(response.body);
@@ -22,7 +22,19 @@ class UserService {
   }
 
   Future<User> login(String email, String password) async{
-        final response = await http.get(Uri.parse('$_baseUrl/events'), headers: <String, String>{'Content-Type': 'application/json', 'Authorization': _auth});
+        final response = await http.post(Uri.parse('$_baseUrl/login'), headers: <String, String>{'Content-Type': 'application/json', 'Authorization': _auth},
+        body: jsonEncode({
+          'email_address': email,
+          'password_hash': password,
+        }),
+        );
+
+          if(response.statusCode == 200) {
+    return User.fromJson(jsonDecode(response.body));
+  }
+
+  throw Exception('Failed to login (${response.statusCode})');
+
 
   }
 
