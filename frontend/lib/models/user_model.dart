171143a0c37e2ac39c7ce008c703ac32e87cec34
Anna Lloyd <lloan016@student.otago.ac.nz>
2026-04-19 16:42:54 +1200

    starting connecting front and back for login functions

---
+++ frontend/lib/models/user_model.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+
+List<User> userFromJson(String str) => List<User>.from(json.decode(str).map((x) => User.fromJson(x)));
+
+class User {
+  final String firstName;
+  final String lastNight;
+  final String email;
+  final String password;
+
+
+  User({
+    required this.firstName,
+    required this.lastNight,
+    required this.email,
+    required this.password,
+
+  });
+
+  factory User.fromJson(Map<String, dynamic> json){
+    return User(
+      firstName: json['first_name'] as String,
+      lastNight: json['last_name'] as String,
+      email: json['email_address'] as String,
+      password: json['password_hash'] as String,
+    );
+  }
+
+}
+
