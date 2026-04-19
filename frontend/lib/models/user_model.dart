763242b24a1a185e0d3403260c31c98993d24e58
Anna Lloyd <lloan016@student.otago.ac.nz>
2026-04-19 20:41:10 +1200

    adding deleting users

--- frontend/lib/models/user_model.dart
+++ frontend/lib/models/user_model.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 List<User> userFromJson(String str) => List<User>.from(json.decode(str).map((x) => User.fromJson(x)));
 
 class User {
+  final String userId;
   final String firstName;
   final String lastNight;
   final String email;
@@ -11,6 +12,7 @@ class User {
 
 
   User({
+    required this.userId,
     required this.firstName,
     required this.lastNight,
     required this.email,
@@ -20,6 +22,7 @@ class User {
 
   factory User.fromJson(Map<String, dynamic> json){
     return User(
+      userId: json['user_id'] as String,
       firstName: json['first_name'] as String,
       lastNight: json['last_name'] as String,
       email: json['email_address'] as String,
